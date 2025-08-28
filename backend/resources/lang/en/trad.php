217d01d8ec7ecb568de565f68dfeb9b31a29e3d8
Sergio Diaz Ramirez <sergio.diazra@alum.uca.es>
2025-08-28 05:19:07 +0200

    feat: add multilanguage

---
+++ backend/resources/lang/en/trad.php
@@ -0,0 +1,6 @@
+<?php
+
+return 
+[
+    'card' => 'Card'
+];
---
+++ backend/resources/lang/es/trad.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'user' => 'Usuario',
+    'email' => 'Correo electrónico',
+    'status' => 'Estado',
+    'card' => 'Tarjeta',
+    'brand' => 'Marca',
+    'last_4' => 'Últimos 4 dígitos',
+    'created_at' => 'Creado el',
+    'updated_at' => 'Actualizado el',
+    'address' => 'Dirección',
+    'order' => 'Pedido',
+    'orders' => 'Pedidos',
+    'product' => 'Producto',
+    'products'     => 'Productos',
+    'quantity'     => 'Cantidad',
+    'weight'       => 'Peso',
+    'price'        => 'Precio',
+    'total_price'  => 'Precio total',
+    'article' => 'Artículo',
+    'pay' => 'Pago',
+    'at_store' => 'En tienda',
+    'order_number' => 'Nº Pedido'
+];
